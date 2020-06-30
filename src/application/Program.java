325c644c303634a6bf2478026f5a265c55d57821
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-06-30 21:58:05 +0100

    Create table in dataBase with JPA and Maven

---
+++ src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import domain.People;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		People p1 = new People(1, "Carlos", "carlos@gmail.com");
+		People p2 = new People(2, "Jorge", "jorge@gmail.com");
+		People p3 = new People(3, "Mario", "mario@gmail.com");
+		
+		System.out.println(p1);
+		System.out.println(p2);		
+		System.out.println(p3);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
